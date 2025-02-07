005e154523773991a9c9ee63c0d660ac5d1991ea
andrii <a.iakumiv@gmail.com>
2025-02-07 21:58:15 +0200

    Module 2-1 function getPrice

---
+++ js/module-2-1.js
@@ -0,0 +1,9 @@
+// function getPrice(subscription) {
+//   let price = 0;
+//   if (subscription === "pro") {
+// 	price = 100;
+//   }
+//   return price;
+// }
+// console.log(getPrice("free")); // 0
+// console.log(getPrice("pro")); // 100
