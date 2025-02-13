e3f54ccd3427c4df02d81fbdfcfc9fc957f50f0a
andrii <a.iakumiv@gmail.com>
2025-02-13 16:25:43 +0200

    Add module for array

---
+++ js/module-03/module-3-1.js
@@ -0,0 +1,25 @@
+// Оголоси змінну fruits. Надай змінній fruits наступне значення: масив фруктів - рядків "apple", "plum", "pear" і "orange".
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// ------------------------------------------------------
+
+/**
+Оголоси три змінні і надай кожній змінній відповідне значення, використовуючи нотацію квадратних дужок.
+Ім'я змінної	Значення змінної
+firstElement	перший елемент масиву
+secondElement	другий елемент масиву
+lastElement	останній елемент масиву
+*/
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// const firstElement = fruits[0];
+// const secondElement = fruits[1];
+// const lastElement = fruits[3];
+
+// console.log(firstElement);
+// console.log(secondElement);
+// console.log(lastElement);
+
+// ------------------------------------------------------
