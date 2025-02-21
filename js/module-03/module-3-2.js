91135284cdf07d596009172c0b0d3860104be83e
andrii <a.iakumiv@gmail.com>
2025-02-21 12:29:27 +0200

    Add method includes

--- js/module-03/module-3-2.js
+++ js/module-03/module-3-2.js
@@ -40,3 +40,31 @@
 // console.log(getEvenNumbers(6, 12)); // [6, 8, 10, 12]
 // console.log(getEvenNumbers(8, 8)); // [8]
 // console.log(getEvenNumbers(7, 7)); // []
+
+/**
+ * ? Метод — includes()
+ * Функція checkStorage(storage, item) приймає два параметри:
+ * storage - масив рядків, що описує доступні товари на складі
+ * item - рядок з назвою товара, наявність якого потрібно перевірити
+ * Доповни код функції таким чином, щоб вона перевіряла, чи присутній
+ * такий товар в масиві storage і повертала:
+ * рядок "<item> is available to order!", де item - це назва товара, якщо товар було знайдено
+ * рядок "Sorry! We are out of stock!", якщо такого товара немає в масиві
+ * Зроби так, щоб пошук за ім'ям товару був незалежний від регістру,
+ * тобто наприклад "plum" і "pLuM" мають бути знайдені у масиві ["apple", "plum", "pear"].
+ */
+
+function checkStorage(storage, item) {
+  const lowerItem = item.toLowerCase();
+  console.log(lowerItem);
+
+  if (storage.includes(lowerItem)) {
+    return `${lowerItem} is available to order!`;
+  } else {
+    return `Sorry! We are out of stock!`;
+  }
+}
+
+console.log(checkStorage(['apple', 'plum', 'pear'], 'plum')); // "plum is available to order!"
+console.log(checkStorage(['apple', 'plum', 'pear'], 'orange')); // "Sorry! We are out of stock!"
+console.log(checkStorage(['apple', 'plum', 'pear'], 'pEAr')); // "pear is available to order!"
