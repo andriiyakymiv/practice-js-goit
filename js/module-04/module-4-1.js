02d47e95e2c9996f36afc557e0f61d955f92b6cc
andrii <a.iakumiv@gmail.com>
2025-02-24 11:10:39 +0200

    Creating an object

---
+++ js/module-04/module-4-1.js
@@ -0,0 +1,17 @@
+/**
+ * ? Створення об'єкта
+ * Оголоси змінну apartment і задай ій об'єкт, який описує квартиру з наступними характеристиками:
+ * imgUrl - рядок, що містить шлях до зображення, значення "https://via.placeholder.com/640x480";
+ * descr - рядок, що містить опис, значення "Spacious apartment in the city center";
+ * rating - число, що містить рейтинг, значення 4;
+ * price - число, що містить ціну, значення 2153;
+ * tags - масив рядків, що містить метаінформацію, значення ["premium", "promoted", "top"].
+ */
+
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+};
