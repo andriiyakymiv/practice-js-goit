5043211864c91daf0b16242d2654bd1edf1d7e87
andrii <a.iakumiv@gmail.com>
2025-02-24 11:37:55 +0200

    Accessing nested properties

--- js/module-04/module-4-1.js
+++ js/module-04/module-4-1.js
@@ -57,15 +57,48 @@
  * aptTags - теги.
  */
 
-const apartment = {
-  imgUrl: 'https://via.placeholder.com/640x480',
-  descr: 'Spacious apartment in the city center',
-  rating: 4,
-  price: 2153,
-  tags: ['premium', 'promoted', 'top'],
-};
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+// };
 
-const aptRating = apartment;
-const aptDescr = apartment;
-const aptPrice = apartment;
-const aptTags = apartment;
+// const aptRating = apartment.rating;
+// const aptDescr = apartment.descr;
+// const aptPrice = apartment.price;
+// const aptTags = apartment.tags;
+
+/**
+ * ? Доступ до вкладених властивостей
+ * Об'єкт apartment описує квартиру 6 властивостями: шлях до зображення, опис, рейтинг, ціна, теги і власник.
+ * Властивість owner (власник) також є об'єктом. Оголошено шість змінніх, значення яких це значення відповідних властивостей об'єкту apartment.
+ * Доповни код, задавши оголошеним змінним вирази звернення до відповідних властивостей об'єкта apartment.
+ * ownerName - ім'я власника;
+ * ownerPhone - телефон власника;
+ * ownerEmail - пошта власника;
+ * numberOfTags - кількість елементів масиву у властивості tags;
+ * firstTag - перший елемент масиву у властивості tags;
+ * lastTag - останній елемент масиву у властивості tags.
+ */
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Henry',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+
+// const ownerName = apartment.owner.name;
+// const ownerPhone = apartment.owner.phone;
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags[0];
+// const lastTag = apartment.tags.pop();
