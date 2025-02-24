097668d8f980c26827885068c85c6c262cbe1813
andrii <a.iakumiv@gmail.com>
2025-02-24 12:08:03 +0200

    Brief properties

--- js/module-04/module-4-1.js
+++ js/module-04/module-4-1.js
@@ -188,3 +188,27 @@
 // apartment.location.city = 'Kingston';
 
 // console.log(apartment);
+
+/**
+ * ? Короткі властивості - Brief properties
+ * Доповни код оголошення об'єкта таким чином, щоб у нього були властивості name, price, image
+ * і tags зі значеннями зі змінних з аналогічними іменами. Обов'язково використовуй синтаксис коротких властивостей.
+ */
+
+// const name = 'Repair Droid';
+// const price = 2500;
+// const image = 'https://via.placeholder.com/640x480';
+// const tags = ['on sale', 'trending', 'best buy'];
+
+// const product = {
+//   name,
+//   price,
+//   image,
+//   tags,
+// };
+
+// console.log(product);
+
+/**
+ * ?
+ */
