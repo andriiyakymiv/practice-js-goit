6db113fe58ad56ef117fbe9243f55115a7ac2001
andrii <a.iakumiv@gmail.com>
2025-02-24 12:01:46 +0200

    Adding properties

--- js/module-04/module-4-1.js
+++ js/module-04/module-4-1.js
@@ -155,3 +155,36 @@
 // apartment.tags.push('trusted');
 
 // console.log(apartment);
+
+/**
+ * ? Додавання властивостей
+ * Додай об'єкту apartment кілька нових властивостей:
+ * area - площа в квадратних метрах, число 60;
+ * rooms - кількість кімнат, число 3;
+ * location - місце розташування квартири, об'єкт з наступними вкладеними властивостями;
+ * country - країна, рядок "Jamaica";
+ * city - місто, рядок "Kingston".
+ * Зверніть увагу: якщо ви спробуєте додати властивості country і city до ще не створеної властивості location,
+ * ви отримаєте помилку. Спочатку ініціалізуйте location як порожній об'єкт, а потім додавайте до нього властивості.
+ */
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ['premium', 'promoted', 'top', 'trusted'],
+//   owner: {
+//     name: 'Henry Sibola',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = {};
+// apartment.location.country = 'Jamaica';
+// apartment.location.city = 'Kingston';
+
+// console.log(apartment);
