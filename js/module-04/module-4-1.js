8f79120af4cd97ca4ccc6ad9d8e08d178dd5ea75
andrii <a.iakumiv@gmail.com>
2025-02-24 11:54:37 +0200

    Changing property values

--- js/module-04/module-4-1.js
+++ js/module-04/module-4-1.js
@@ -126,3 +126,32 @@
 // const aptDescr = apartment['descr'];
 // const aptPrice = apartment['price'];
 // const aptTags = apartment['tags'];
+
+/**
+ * ? Зміна значення властивостей
+ * Доповни код, оновивши значення властивостей об'єкта apartment:
+ * ціну у властивості price на 5000;
+ * рейтинг квартири у властивості rating на 4.7;
+ * ім'я власника у вкладеній властивості name на "Henry Sibola";
+ * масив тегів у властивості tags, додавши в кінець рядок "trusted".
+ */
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Henry',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+
+// apartment.price = 5000;
+// apartment.rating = 4.7;
+// apartment.owner.name = 'Henry Sibola';
+// apartment.tags.push('trusted');
+
+// console.log(apartment);
