b363b126ead468040d3c900c8d49168a9282fda1
andrii <a.iakumiv@gmail.com>
2025-02-24 15:00:26 +0200

    Add object search

--- js/module-04/module-4-2.js
+++ js/module-04/module-4-2.js
@@ -1,1 +1,26 @@
+/**
+ * ? Перебір об'єкта - Object search
+ * Перебери об'єкт apartment, використовуючи цикл for...in, і запиши в масив keys всі його ключі,
+ * а в масив values всі значення його властивостей.
+ */
 
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+
+// const keys = [];
+// const values = [];
+
+// for (const key in apartment) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+// }
+
+// console.log(keys);
+// console.log(values);
+
+/**
+ * ? Метод Object.keys()
+ */
