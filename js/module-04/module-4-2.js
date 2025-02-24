3776f8e6220d8a8ac46b5a46239d66f0931f4903
andrii <a.iakumiv@gmail.com>
2025-02-24 15:19:28 +0200

    Add method object keys

--- js/module-04/module-4-2.js
+++ js/module-04/module-4-2.js
@@ -23,4 +23,21 @@
 
 /**
  * ? Метод Object.keys()
+ * Перебери об'єкт apartment, використовуючи метод Object.keys() і цикл for...of.
+ * Запиши у змінну keys масив ключів властивостей об'єкта apartment, і додай в масив values всі значення його властивостей.
  */
+
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// const keys = Object.keys(apartment);
+
+// for (const key of keys) {
+//   values.push(apartment[key]);
+// }
+
+// console.log(values);
+// console.log(keys);
