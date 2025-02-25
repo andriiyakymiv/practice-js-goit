9907864d7e60c8dbfaea14250bafdf8c38edf42b
andrii <a.iakumiv@gmail.com>
2025-02-25 11:07:43 +0200

    Collection of property values

--- js/module-04/module-4-3.js
+++ js/module-04/module-4-3.js
@@ -56,3 +56,32 @@
 // console.log(getProductPrice('Scanner'));
 // console.log(getProductPrice('Droid'));
 // console.log(getProductPrice('Engine'));
+
+/**
+ * ? Колекція значень властивості - Collection of property values
+ * Напиши функцію getAllPropValues(propName), яка приймає один параметр propName - ім'я (ключ) властивості.
+ * Функція повинна повернути масив усіх значень властивості з таким ім'ям з кожного об'єкта в масиві products.
+ * Якщо в об'єктах відсутні властивості з таким ім'ям, функція повинна повернути порожній масив.
+ */
+
+// function getAllPropValues(propName) {
+//   const products = [
+//     { name: 'Radar', price: 1300, quantity: 4 },
+//     { name: 'Scanner', price: 2700, quantity: 3 },
+//     { name: 'Droid', price: 400, quantity: 7 },
+//     { name: 'Grip', price: 1200, quantity: 9 },
+//   ];
+
+//   let getValues = [];
+
+//   for (const product of products) {
+//     if (product[propName] !== undefined) {
+//       getValues.push(product[propName]);
+//     }
+//   }
+//   return getValues;
+// }
+
+// console.log(getAllPropValues('name')); // ["Radar", "Scanner", "Droid", "Grip"]
+// console.log(getAllPropValues('quantity')); // [4, 3, 7, 9]
+// console.log(getAllPropValues('price')); // [1300, 2700, 400, 1200]
