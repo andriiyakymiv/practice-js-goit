2d67620d34d8311bb4d130a1bc31f086b1ac1245
andrii <a.iakumiv@gmail.com>
2025-02-25 15:09:33 +0200

    Method object

---
+++ js/module-04/module-4-4.js
@@ -0,0 +1,19 @@
+/**
+ * ? Методи об'єкта
+ * До нас звернулася власниця крамниці зілля «У старої жаби» і замовила програму для ведення інвентарю.
+ * Програма має додавати, видаляти, шукати та оновлювати зілля.
+ * Оголоси об'єкт atTheOldToad з наступними властивостями:
+ * potions — масив, де будуть зберігатися зілля. Нехай зараз він буде порожнім
+ * getPotions() — метод, який повертає рядок "List of all available potions"
+ * addPotion(potionName) — метод, який повертає рядок "Adding <potionName>", де potionName — це значення параметра potionName
+ */
+
+// const atTheOldToad = {
+//   potions: [],
+//   getPotions() {
+//     return `List of all available potions`;
+//   },
+//   addPotion(potionName) {
+//     return `Adding ${potionName}`;
+//   },
+// };
