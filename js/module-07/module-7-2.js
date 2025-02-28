a7a0d139778e086f73ce8debb2bef2cdf3e3dd8f
andrii <a.iakumiv@gmail.com>
2025-02-28 20:30:39 +0200

    Custom attributes data

--- js/module-07/module-7-2.js
+++ js/module-07/module-7-2.js
@@ -15,3 +15,9 @@ console.log(link.classList);
 
 link.classList.replace('special', 'regular');
 console.log(link.classList);
+
+const saveBtn = document.querySelector('.editor button[data-action="save"]');
+const closeBtn = document.querySelector('.editor button[data-action="close"]');
+
+console.log(saveBtn.dataset.action); //save
+console.log(closeBtn.dataset.action); //close
