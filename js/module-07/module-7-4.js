4a8cb780eeaabc20054179716a4bc0e1883f8034
andrii <a.iakumiv@gmail.com>
2025-03-02 11:26:54 +0200

    Method addEventListener

--- js/module-07/module-7-4.js
+++ js/module-07/module-7-4.js
@@ -2,3 +2,17 @@
  * ? Події
  * ? Метод addEventListener()
  */
+
+// const button = document.querySelector('.my-button');
+
+// button.addEventListener('click', () => {
+//   console.log('The button was pressed and now the next image will appear');
+// });
+
+// const button = document.querySelector('.my-button');
+
+// const handleClick = () => {
+//   console.log('The button was pressed and now the next image will appear');
+// };
+
+// button.addEventListener('click', handleClick);
