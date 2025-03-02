9abaa316ccf9d3587487d57e7ad47cc85c5eb7d0
andrii <a.iakumiv@gmail.com>
2025-03-02 11:31:37 +0200

    Method removeEventListener

--- js/module-07/module-7-4.js
+++ js/module-07/module-7-4.js
@@ -16,3 +16,16 @@
 // };
 
 // button.addEventListener('click', handleClick);
+
+/**
+ * ? Події
+ * ? Метод removeEventListener()
+ */
+
+// const button = document.querySelector('.my-button');
+
+// const handleClick = () => {
+//   console.log('The button was pressed and now the next image will appear');
+// };
+
+// button.addEventListener('click', handleClick);
