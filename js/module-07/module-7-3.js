5d064a18da70d5eff9e0eeb483d36fb2dd3dea51
andrii <a.iakumiv@gmail.com>
2025-03-02 09:55:46 +0200

    Add create element

--- js/module-07/module-7-3.js
+++ js/module-07/module-7-3.js
@@ -1,1 +1,17 @@
-document.createElement(tagName);
+/**
+ * ? Створення елементів
+ */
+// document.createElement(tagName);
+// const heading = document.createElement('h1');
+// const heading = document.createElement('h1');
+// headding.classList.add('title');
+// heading.textContent = 'This is a heading';
+// console.log(heading); // <h1 class="title">This is a heading</h1>
+
+// const image = document.createElement('img');
+// image.src = 'https://picsum.photos/id/11/320/240';
+// image.alt = 'Nature';
+// console.log(image); // <img src="https://picsum.photos/id/11/320/240" alt="Nature" />
+
+// const div = document.createElement('div');
+// console.log(div);
