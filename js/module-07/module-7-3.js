adc31073b835af04155319189d32c6e5e851667c
andrii <a.iakumiv@gmail.com>
2025-03-02 10:34:52 +0200

    Add method insertAdjacentHTML

--- js/module-07/module-7-3.js
+++ js/module-07/module-7-3.js
@@ -41,3 +41,17 @@
 // const firstElement = document.createElement('li');
 // firstElement.textContent = 'Ajax';
 // list.prepend(firstElement);
+
+/**
+ * ? Метод insertAdjacentHTML()
+ */
+
+// const list = document.querySelector('.list');
+
+// const newTechnologies = ['React', 'TypeScript', 'Node.js'];
+// const markup = newTechnologies
+//   .map(technology => `<li class="list-item new">${technology}</li>`)
+//   .join('');
+
+// list.insertAdjacentHTML('beforeend', markup);
+// list.insertAdjacentHTML('beforebegin', '<h2>Popular technologies</h2>');
