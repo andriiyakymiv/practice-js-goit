349516300f403fbb1980b3b17ac362171acb58f3
andrii <a.iakumiv@gmail.com>
2025-03-03 08:12:22 +0200

    Add event submit

--- js/module-07/module-7-5.js
+++ js/module-07/module-7-5.js
@@ -1,1 +1,23 @@
-console.log('Події елементів форм');
+// console.log('Події елементів форм');
+// const form = document.querySelector('form');
+
+// form.addEventListener('submit', event => {
+// });
+
+const registerForm = document.querySelector('.form');
+
+registerForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const login = form.elements.login.value;
+  const password = form.elements.password.value;
+
+  if (login === '' || password === '') {
+    return console.log('Please fill in all the fields!');
+  }
+
+  console.log(`Login: ${login}, Password: ${password}`);
+  form.reset();
+}
