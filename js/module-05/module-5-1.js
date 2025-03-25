0939ab2ec6cf959f09f4047193c15ede61e0a291
andrii <a.iakumiv@gmail.com>
2025-03-25 21:36:01 +0200

    Add function deliverPizza  module five

--- js/module-05/module-5-1.js
+++ js/module-05/module-5-1.js
@@ -9,3 +9,39 @@
 
 // console.log(result);
 // console.log(pointer);
+
+// 2
+
+// function greet(name) {
+//   console.log(`Welcome ${name}!`);
+// }
+
+// function notify(name) {
+//   console.log(`Dear ${name}, your room will be ready in 30 minutes`);
+// }
+
+// function registerGuest(name, callback) {
+//   console.log(`Registering ${name}!`);
+//   callback(name);
+// }
+
+// registerGuest('Mango', greet);
+
+// registerGuest('Mango', notify);
+
+// 3
+
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
+}
+
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
+
+function makeMessage(pizzaName, callback) {
+  return callback(pizzaName);
+}
+
+console.log(makeMessage('Royal Grand', makePizza));
+console.log(makeMessage('Ultracheese', deliverPizza));
