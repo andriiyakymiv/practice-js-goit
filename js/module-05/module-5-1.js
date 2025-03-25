425ec1d817c4efd591f566cf4a5dfd7aae98393b
andrii <a.iakumiv@gmail.com>
2025-03-25 21:07:49 +0200

    Add function makePizza module five

---
+++ js/module-05/module-5-1.js
@@ -0,0 +1,11 @@
+// 1
+
+// function makePizza() {
+//   return 'Your pizza is being prepared, please wait.';
+// }
+
+// const result = makePizza();
+// const pointer = makePizza;
+
+// console.log(result);
+// console.log(pointer);
